0718639b08f0521ec15841f23d9217c16f2c7dc4
C.C. Leonard <67921480+ccleonard@users.noreply.github.com>
2020-07-21 17:26:44 -0400

    initial commit

---
+++ tipit/src/components/header.js
@@ -0,0 +1,26 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header>
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
