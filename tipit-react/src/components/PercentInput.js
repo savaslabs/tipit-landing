3e608775ee8e42ac5f01bb2f1fb77da20f222fd2
maddybvh <maddybvh@gmail.com>
2020-08-03 12:50:24 -0400

    Issue #26754645: Adjust percent input size

--- tipit-react/src/components/PercentInput.js
+++ tipit-react/src/components/PercentInput.js
@@ -1,24 +1,40 @@
-import React, {useContext} from 'react';
-import AppContext from './AppContext';
-import styles from '../theme/appstyles.module.css';
+import React, { useContext } from "react";
+import AppContext from "./AppContext";
+import styles from "../theme/appstyles.module.css";
 
-export const PercentInput = ({ value, onChange, ...props}) => {
-    const context = useContext(AppContext)
-    const { colors } = context.useTheme()
-    return (
-        <React.Fragment>
-            <div style={{display: 'flex', alignItems: 'baseline', maxWidth: 40, marginRight: 15, color: colors.text}}>
-                <input 
-                    style={{minWidth: 40, paddingRight: 20, textAlign: 'right', backgroundColor: colors.background, color: colors.text}}
-                    type="number"
-                    min="0"
-                    max="100"
-                    step="1"
-                    value={value}
-                    onChange={e => onChange(e.target.value)}
-                    />
-                <span className={styles.label} style={{marginLeft: -20}}>%</span>
-            </div>
-        </React.Fragment>
-    )
-}
+export const PercentInput = ({ value, onChange, ...props }) => {
+  const context = useContext(AppContext);
+  const { colors } = context.useTheme();
+  return (
+    <React.Fragment>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "baseline",
+          maxWidth: 60,
+          marginRight: 15,
+          color: colors.text,
+        }}
+      >
+        <input
+          style={{
+            minWidth: 40,
+            paddingRight: 20,
+            textAlign: "right",
+            backgroundColor: colors.background,
+            color: colors.text,
+          }}
+          type="number"
+          min="0"
+          max="100"
+          step="1"
+          value={value}
+          onChange={e => onChange(e.target.value)}
+        />
+        <span className={styles.label} style={{ marginLeft: -20 }}>
+          %
+        </span>
+      </div>
+    </React.Fragment>
+  );
+};
