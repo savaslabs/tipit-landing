a09acd5c50228469b07feb10fe15f5c66e6adeae
maddybvh <maddybvh@gmail.com>
2020-08-05 11:36:36 -0400

    Updates

---
+++ precache-manifest.748f2a586a45eceb54f6eb78350a67e2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6f64e76b7b975d3a4c0825efc247ac",
+    "url": "/tipit-landing/index.html"
+  },
+  {
+    "revision": "d7d4ff67df290a5f2fb6",
+    "url": "/tipit-landing/static/css/main.1197302c.chunk.css"
+  },
+  {
+    "revision": "8240c059ce5c63a2c7b9",
+    "url": "/tipit-landing/static/js/2.5f1a1b83.chunk.js"
+  },
+  {
+    "revision": "b948d2a7727b83687a00ec53ef5267c0",
+    "url": "/tipit-landing/static/js/2.5f1a1b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d4ff67df290a5f2fb6",
+    "url": "/tipit-landing/static/js/main.aff09e0c.chunk.js"
+  },
+  {
+    "revision": "b9382f343e820437dfe6",
+    "url": "/tipit-landing/static/js/runtime-main.92b91ad6.js"
+  },
+  {
+    "revision": "256311d1b65d983e6a5ad9bb5838dca5",
+    "url": "/tipit-landing/static/media/ava-blue.256311d1.svg"
+  },
+  {
+    "revision": "69f83b20ece2f66f6aaca6e6b4170af0",
+    "url": "/tipit-landing/static/media/dashes-white.69f83b20.svg"
+  },
+  {
+    "revision": "188e66f7a1f0c5932fc999ca9a7d9481",
+    "url": "/tipit-landing/static/media/dashes.188e66f7.svg"
+  },
+  {
+    "revision": "023d2d3e007eef2bbf53da2b8208d1bc",
+    "url": "/tipit-landing/static/media/info-white.023d2d3e.svg"
+  },
+  {
+    "revision": "a21bafa47a3bc0f265c3a439da699d4e",
+    "url": "/tipit-landing/static/media/info.a21bafa4.svg"
+  },
+  {
+    "revision": "c7c96ed0cc3ba94058954a43207300ba",
+    "url": "/tipit-landing/static/media/labs.c7c96ed0.svg"
+  },
+  {
+    "revision": "66722f83de0d313119d49ec839d1435a",
+    "url": "/tipit-landing/static/media/savas-logo-name.66722f83.svg"
+  },
+  {
+    "revision": "89059317af4643570e57ffa95018dddb",
+    "url": "/tipit-landing/static/media/settings-white.89059317.svg"
+  },
+  {
+    "revision": "6ae6bc54cfd015f10a2ae6a9f2a43fab",
+    "url": "/tipit-landing/static/media/settings.6ae6bc54.svg"
+  },
+  {
+    "revision": "af3523aa80569b7d910dfc5392206224",
+    "url": "/tipit-landing/static/media/x-close-pink.af3523aa.svg"
+  },
+  {
+    "revision": "749fc1ce6ff5bf6076ed465cc8448734",
+    "url": "/tipit-landing/static/media/x-close.749fc1ce.svg"
+  }
+]);
